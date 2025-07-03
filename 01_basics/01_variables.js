0f934d3e2ff4085aed084f305b6eaee2b53241ce
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-03 07:25:50 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=144553
+let ammountEmail="abhishek@gmail"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=2  not allowed
+ammountEmail="njervjkre@gmail"
+acoountPassword="000009"
+accountCity="sasaram"
+
+
+
+// console.log(accountId);
+// console.log(ammountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId,ammountEmail,accountPassword,accountCity])
+
+
+
