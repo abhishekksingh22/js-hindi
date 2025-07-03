609135cbb987ea0ea4b0f55d59890a8b7fee0763
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-03 16:18:15 +0000

    coversionofdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = 33;  //it is a integer
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let vauleInNumber=Number(score);//convert string into number
+// console.log(typeof vauleInNumber);
+// console.log( vauleInNumber);
+
+//number "33"=>33
+//"33abc"=>NAN
+//"TRUE"=>1
+//FALSE=>0
+//NULL=>0
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=> flase
+//""=>false
+//"name"=> true
+
+let someNumber=33
+let Stringnumber=String(someNumber);
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
+
+
+
+
+
+
+
