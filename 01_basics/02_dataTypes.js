a6994fd985b591c7efb5f79e8166994add2a0242
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-03 09:23:22 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,3 +6,31 @@ console.log(3+
 
 console.log("sintu");
 
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state;
+
+//num=>2 to power 53
+//bigInt
+//string=> ""
+//boolean=true/false
+//null=>strandalone vaule
+//undefined=>not assigned vaule yet
+//symbol => unique
+
+
+//object
+
+  console.log(typeof "hitesh");
+  console.log(typeof 18);
+
+
+
+  console.log(typeof undefined);  //undefined
+  console.log(typeof null);      //object
+  
+
+
+
+
