717cebff857f395bfb8d287c4ad99e6a8a05ecc0
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-05 17:35:22 +0000

    comparision

--- 01_basics/04_comparasion.js
+++ 01_basics/04_comparasion.js
@@ -10,5 +10,15 @@ console.log("02" > 1);
 console.log(null>0);
 console.log(null<0);
 console.log(null>=0);
+                         //avoid these comparision clean code
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+//=== check vaules and datatypes
+
+console.log("2"==2);// changes datatypes doesnot check datatypes so true
+console.log("2"===2);//check vaules and datatypes so false
+
 
 
