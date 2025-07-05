2ae76a4abaedd4d98d701c220f720c859f3bb457
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-05 09:44:19 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -46,17 +46,21 @@ let negVaule=-vaule
 // console.log(2/2);
 // console.log(2%2);
 
-let str1="hello"
-let str2=" hitesh"
+// let str1="hello"
+// let str2=" hitesh"
 
-let str3 =  str1 + str2
-console.log(str3);
+// let str3 =  str1 + str2
+// console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+"2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+
+
 
 
 
