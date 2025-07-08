38aa787d03eb6db03ba4351f281630435159ea4d
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-08 07:49:59 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -7,9 +7,30 @@ const scoreVaule =100.3;
 
 const loggedIn=false;
 const outsideTemp=null;
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+const BigNumber=1511318541331135n
+
 
 
 
 //Reference (Non primitive)
 
 //Array,Objects,Functions
+const heroes[]=["shaktiman","naagraj","doga"]
+let myobj= {
+    name:"sintu",
+    age:20
+
+}
+
+const myFunction =function()
+{
+    console.log("Hello World");
+    
+
+}
