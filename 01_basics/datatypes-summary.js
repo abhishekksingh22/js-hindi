8342ad77f0da3bd91f2c6a9463084f8a5a7bb40b
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-08 07:00:50 +0000

    datatypees

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,5 +2,14 @@
 
 //7 types : String,Number,Boolean,null,undefined,Symbol
 // BigInt
+const score=100;
+const scoreVaule =100.3;
+
+const loggedIn=false;
+const outsideTemp=null;
+
+
 
 //Reference (Non primitive)
+
+//Array,Objects,Functions
