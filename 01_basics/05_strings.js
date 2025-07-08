7b09d650f72a1284d99e580d3f88c372f0c09939
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-08 10:34:20 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,9 +1,23 @@
-const name="sintu"
-const repoCount=50
+const name = "sintu";
+const repoCount = 50;
 
-console.log(name+ repoCount+" Value ");
+console.log(name + repoCount + " Value "); // Output: sintu50 Value
 
-const gameName=new String('sintupc') 
-console.log(gameName.length);
+const gameName = new String('sintu-pc');
+
+console.log(gameName.length);       // Output: 7
+console.log(gameName.charAt(4));    // Output: u
+console.log(gameName.indexOf('u'));
+
+const newString=gameName.substring(0,5)
+console.log(newString);
+
+const anotherString=gameName.slice(0,4)
+console.log(anotherString);
+
+const newStringOne="  sintu    " //extra space
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
 
 
