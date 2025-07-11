44e165bd224a9533fc3ca817217aeef7ff9db1ff
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-11 13:11:50 +0000

    maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,28 +1,50 @@
-const score=400
-console.log(score);
-console.log(typeof score);
+// const score=400
+// console.log(score);
+// console.log(typeof score);
 
-const balance=new Number(100)
-console.log(balance);
+// const balance=new Number(100)
+// console.log(balance);
 
 
 
-console.log(balance.toString().length); 
-console.log(balance.toFixed(2));//100.00 
-//console.log(typeof balance);
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2));//100.00 
+// //console.log(typeof balance);
 
-const otherNumber=123.8966;
-console.log(otherNumber.toPrecision(3));// 
+// const otherNumber=123.8966;
+// console.log(otherNumber.toPrecision(3));// 
 
-const hundreds=1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
 
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_VALUE);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
 
 //+++++++++++++++++ Maths +++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(6.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4,2,9,3));
+// console.log(Math.min(4,2,9,3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min )
+
+
+
+
+
 
 
 
