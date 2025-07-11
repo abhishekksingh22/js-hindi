50b130c410e36d2b9ca9e3e905d3cd60e6839ab9
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-11 12:11:41 +0000

    number

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -6,15 +6,29 @@ const balance=new Number(100)
 console.log(balance);
 
 
+
 console.log(balance.toString().length); 
-console.log(balance.toFixed(2)); 
+console.log(balance.toFixed(2));//100.00 
 //console.log(typeof balance);
 
 const otherNumber=123.8966;
-console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(3));// 
 
 const hundreds=1000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+//+++++++++++++++++ Maths +++++++++++++++++++//
+
+
+
+
+
+
 
 
 
