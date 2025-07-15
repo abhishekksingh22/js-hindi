fc942c0fbbd9ff44d5daffe8a6d88685a14c4366
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-15 10:09:42 +0000

    dates

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,13 +1,36 @@
-let myDate=new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.getDate());
-console.log(myDate.toLocaleDateString);
-myDate.setMinutes(24)
-console.log(myDate.getMinutes());
+// let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.getDate());
+// console.log(myDate.toLocaleDateString);
+// myDate.setMinutes(24)
+// console.log(myDate.getMinutes());
 
-let myCreatedDate=new Date(2023,0,23)
-console.log(myCreatedDate);
+//let myCreatedDate=new Date(2023,0,23)
+let myCreatedDate=new Date("2005-05-22")
+//let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());
+
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+
+
+// let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate.toLocaleString());
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+})
+
+
+
+
 
 
 
