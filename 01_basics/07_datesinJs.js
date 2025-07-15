db1445b2853047b07cfd99c9d0c2768bcd2282fe
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-15 09:14:57 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,15 @@
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.getDate());
+console.log(myDate.toLocaleDateString);
+myDate.setMinutes(24)
+console.log(myDate.getMinutes());
+
+let myCreatedDate=new Date(2023,0,23)
+console.log(myCreatedDate);
+
+
+
+
+
