49066b54166af2e8b1bdb4772a94c1b4b34a0fb0
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-17 15:54:21 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,21 @@
+const marvel_heroes=["thor","Ironman","spiderman"]
+const dc_heroes=["superman","flash","batman"]
+//marvel_heroes.push(dc_heroes);
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[0]);
+// console.log(marvel_heroes[3]);
+
+const allHeroes=marvel_heroes.concat(dc_heroes);
+//console.log(allHeroes);
+
+const all_new_heroes=[...marvel_heroes,...dc_heroes]
+console.log(all_new_heroes);
+
+
+const another_array =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+ 
+
+
+
+
