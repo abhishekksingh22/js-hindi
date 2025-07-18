17153334afa85ea106667097b5e927b8ac9d8c6b
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-18 16:15:09 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,20 @@
+//singleton (made with  constructor)
+
+//object literals
+//object.create
+const Jsuser={
+    name:"hitesh",
+    "full name":"Abhishek kumar singh",
+    age:18,
+    location:"jaipur",
+    email:"hitesh123@gmail.com",
+    isLoggedIn: false,
+    lastLogIn:["mon","sat"]
+}
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+
+
+
+
