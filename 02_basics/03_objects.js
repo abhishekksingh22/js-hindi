ff2b2c7d0282e640105d7a8d77cfb6c72f39eac8
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-07-18 16:54:36 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,18 +2,46 @@
 
 //object literals
 //object.create
+const mySym=Symbol("key1")
+
 const Jsuser={
     name:"hitesh",
     "full name":"Abhishek kumar singh",
+    //mySym:"key1" it is wrong to declare symbol
+    [mySym]:"mykey1",
+
     age:18,
     location:"jaipur",
     email:"hitesh123@gmail.com",
     isLoggedIn: false,
     lastLogIn:["mon","sat"]
 }
-console.log(Jsuser.email);
-console.log(Jsuser["email"]);
-console.log(Jsuser["full name"]);
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["age"]);
+// console.log(Jsuser["full name"]);
+// console.log(Jsuser["mySym"]);
+// console.log(typeof [mySym]);
+
+Jsuser.email="chatgpt";
+//Object.freeze(Jsuser);// iske bad vaule change nhi hota
+
+Jsuser.greeting=function()
+{
+    console.log("hello js user");
+    
+}
+Jsuser.greeting2=function()
+{
+    console.log('hello js user ,${this.name}')
+    
+}
+console.log(Jsuser.greeting());
+console.log(Jsuser.greeting2());
+
+
+
+
 
 
 
