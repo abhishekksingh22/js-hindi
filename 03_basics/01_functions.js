61f7fbd4fd91dbaf63e5bb3d2ddd933fccb8a4f5
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-02 07:58:52 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,18 +10,25 @@ console.log("S");
 console.log("H");
 
 }
-saveMyname(); //calling the functions
+//saveMyname(); //calling the functions
 // saveMyname //calling the reference
 
 function addTwoNumbers(number1,number2)
 {
     let result=number1+number2;
     return result;
+    console.log("hitesh");//result k bad kuch bhi execute nhi hoga
+    
     
 }
 
 const r= addTwoNumbers(7,8);
-console.log("result",r);
+//console.log("result",r);
+
+function loginUserMessage()
+{
+    return '${} just logged  '
+}
 
 
 
