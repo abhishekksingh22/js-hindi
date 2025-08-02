fe74079c68090555bda5767b8f3f50bc5c04ddb5
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-02 07:29:51 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+
+
+function saveMyname()
+{
+    console.log("h");
+console.log("I");
+console.log("T");
+console.log("E");
+console.log("S");
+console.log("H");
+
+}
+saveMyname(); //calling the functions
+// saveMyname //calling the reference
+
+function addTwoNumbers(number1,number2)
+{
+    
+}
+
