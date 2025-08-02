bcfcc1529bac8894ad00a46329a6867f89ead61f
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-02 09:10:10 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -45,3 +45,18 @@ function calculateCartPrice(val1,val2,...num1)
     return num1
 }
 console.log(calculateCartPrice(200,400,500));
+
+const user={
+    username:"hitesh",
+    price:199
+}
+function handleObject(anyObject)
+{
+   console.log(`Username is ${anyObject.username}and price is ${anyObject.price} `);
+   
+}
+//handleObject(user);
+handleObject({
+    username:"sintu",
+    price:299
+})
