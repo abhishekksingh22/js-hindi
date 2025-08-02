429caeb51f9cfb30695d5cb788b56ead06985039
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-02 11:10:32 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,29 @@
+// let a=10;
+// const b=20
+// var c=30
+
+//var c=300
+let a=90;
+const b=45
+
+
+if(true) 
+    {
+let a=10;
+const b=20
+var c=30
+
+console.log("vaules inside the if statement");
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+}
+var c=25
+
+console.log("vaules outside the if statement");
+
+console.log(a);
+console.log(b);
+console.log(c);
