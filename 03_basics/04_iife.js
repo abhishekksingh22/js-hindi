b41a6b16cdf04b320e7a43256ceaf8dcac2e9957
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-06 10:14:04 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,7 +1,7 @@
 //Immediatly Invoked Function Expression(IIFE)
 
 
-(function chai()
+(function chai()      //NAMED IIFE
 {
     console.log(`DB CONNECTED`);
 })();
@@ -9,6 +9,12 @@
 (function aurcode()
 {
     console.log(`DB CONNECTED`);
-})()
+})();
+
+((name)=>      //UNNAMED IIFE WITH PASSING PARAMETER
+{
+    console.log(`DB CONNECTED ${name}`);
+})("sintu")
+
 
 
