b476bdf69918357be6ac91beaffaea4a8aa118fe
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-11 10:11:22 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,21 @@
+//if
+// const isUserloggedIn=true;
+
+// if(2==="2")
+// {
+//     console.log("it is equal")
+
+// }
+// else{
+//     console.log("it is not equal");
+    
+// }
+
+// > , < ,<= ,>= ,== ,!= ,=== ,!==
+const score=200;
+
+if(score>100)
+{
+    const power="fly"
+    log
+}
