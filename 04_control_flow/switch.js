992230d919d9f5976110bbb685d40c55d4f4d6f3
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-14 08:29:26 +0000

    controlflow

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -6,3 +6,30 @@
 //     default:
 //         break;
 // }
+const month=3;
+
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        
+        break;
+        case 2:
+            console.log("february");
+            break;
+            
+            case 3:
+                console.log("march");
+                break;
+            case 4:
+                console.log("april");
+                break;
+            
+                
+            
+
+    default:
+        console.log("plese enter vaule from 1 to 12");
+        
+        break;
+}
