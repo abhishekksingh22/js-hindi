049480ae3949a419930960f096f5a0be31a74ebc
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-14 09:47:09 +0000

    loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,29 @@
 //for
 
 for (let index = 0; index < 10; index++) {
-    console.log(index);
+    //console.log(index);
+    if(index==5)
+    {
+        //console.log("5 is best number");
+        
+    }
     
     
 }
+// console.log(index); declare inside braket so it will not work
+
+for(let i=0;i<=10;i++)
+{
+    console.log();
+    
+    console.log(`Outerloop vaule ${i}`);
+    console.log();
+    
+    
+    for(let j=0;j<=10;j++)
+    {
+        console.log(`Innerloop vaule ${j}`);
+
+    }
+}
+
