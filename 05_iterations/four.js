1c833349022067e21738edeac8752e9a49df8959
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-17 08:10:21 +0000

    forEachLoop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,6 +6,26 @@ const myObject={
 }
 
 for (const key in myObject) {
-    console.log(key+"  vaule"+myObject[key]);
+    //console.log(key+"  vaule"+myObject[key]);
+    
+}
+
+const programming=["js","rb","py","java","cpp"]
+for (const key in programming) {
+    //console.log(key+"  "+programming[key]);
+    
+}
+
+//map
+
+const map=new Map()
+map.set("india",4)
+map.set("usa",4)
+map.set("a",1)
+map.set("b",2)
+map.set("c",3)
+//console.log(map);
+for (const key in map) {
+    console.log(key+" "+map[key]);//not iterable in loop
     
 }
