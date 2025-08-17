3243b10e5499942278c2559ab0bd77e7aaebebfc
abhishekksingh22 <abhisheksingh22595@gmail.com>
2025-08-17 09:56:10 +0000

    filtermaps

---
+++ 05_iterations/six.js
@@ -0,0 +1,27 @@
+// const coding=["js","ruby","java","python","cpp"]
+// const vaules=coding.forEach(items => {
+//     console.log(items);
+    
+// });
+// console.log(vaules);
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+//let newnums=myNums.filter((num)=>num>4)
+
+// let newnums=myNums.filter((num)=>{
+//     let w= num>4
+//     return w;
+
+
+// })
+
+// const newnums=[]
+// myNums.forEach((num)=>{
+//     if(num>4)
+//     {
+//         newnums.push(num)
+//     }
+// })
+//console.log(newnums);
+
